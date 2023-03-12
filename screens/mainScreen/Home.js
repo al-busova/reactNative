1ba236fc0df73e7cc72583ce25f2a599e8597eb7
Alena Busova <96522313+al-busova@users.noreply.github.com>
2023-03-12 16:21:02 +0200

    add styles tabs navigation

--- screens/mainScreen/Home.js
+++ screens/mainScreen/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import PostsScreen from "./PostsScreen";
 import CreatePostsScreen from "./CreatePostsScreen";
@@ -10,38 +11,45 @@ const Tabs = createBottomTabNavigator();
 
 export default function Home() {
   return (
-    <Tabs.Navigator screenOptions={{ tabBarShowLabel: false }}>
+    <Tabs.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#FF6C00",
+        tabBarInactiveTintColor: "rgba(33, 33, 33, 0.8)",
+        tabBarStyle: {
+          paddingHorizontal: 45,
+          height: 83,
+          paddingBottom: 34,
+          paddingTop: 9,
+        },
+      }}
+    >
       <Tabs.Screen
         options={{
-          tabBarIcon: ({ focused, size, color }) => (
+          tabBarIcon: ({ size, color }) => (
             <AntDesign name="appstore-o" size={size} color={color} />
           ),
           headerRight: () => (
             <MaterialIcons name="logout" size={24} color="gray" />
-            //   <DrawerButton onPress={() => navigation.toggleDrawer()} />
           ),
-          // tabBarIconStyle: { color:'orange'},
         }}
         name="Публикации"
         component={PostsScreen}
       />
       <Tabs.Screen
-        options={{
-          tabBarIcon: ({ focused, size, color }) => (
-            <AntDesign
-              name="pluscircle"
-              size={32}
-              color="#FF6C00"
-              // iconStyle={{ padding: 20 }}
-            ></AntDesign>
+        options={({ navigation }) => ({
+          tabBarButton: () => (
+            <TouchableOpacity style={styles.iconPlus} activeOpacity={0.7} onPress={() => navigation.navigate('Создать публикацию')}>
+              <AntDesign name="plus" size={16} color="white" />
+            </TouchableOpacity>
           ),
-        }}
+        })}
         name="Создать публикацию"
         component={CreatePostsScreen}
       />
       <Tabs.Screen
         options={{
-          tabBarIcon: ({ focused, size, color }) => (
+          tabBarIcon: ({ size, color }) => (
             <AntDesign name="user" size={size} color={color} />
           ),
           headerShown: false,
@@ -52,3 +60,20 @@ export default function Home() {
     </Tabs.Navigator>
   );
 }
+
+const styles = StyleSheet.create({
+  tabsContainer: {
+    alignItems: "center",
+    gap: 10,
+  },
+  iconPlus: {
+    height: 40,
+    width:70,
+    paddingHorizontal: 25,
+    paddingVertical: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FF6C00",
+    borderRadius: 40,
+  },
+});
